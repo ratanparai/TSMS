74fc220ae42d67366eb084d96f7b370985f8a5bb
Ratan Sunder Parai <ratanparai@gmail.com>
2017-05-22 09:12:42 +0600

    add default scaffolding

---
+++ TSMS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TSMS.MvcApplication" Language="C#" %>
---
+++ TSMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TSMS.Startup))]
+namespace TSMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
